09ce78eb71c49c9218ef1d779faab48eacf6fe68
Vitor Domingos Oliveira de Jesus <47833820857@info.local>
2026-03-04 22:02:30 -0300

    Funcao cosseno finalizada

--- senaimath.py
+++ senaimath.py
@@ -1,4 +1,7 @@
 def fatorial(numero):
+    if numero == 0:
+        return 1
+
     valor = numero
     for _ in range(1, numero):
         valor *= numero - 1
@@ -8,4 +11,16 @@ def fatorial(numero):
 def ang_radiano(angulo):
     radiano = angulo * 3.1415 / 180
     return radiano
+
+def cosseno(grau, termos):
+    radiano = ang_radiano(grau)
+    cosseno = 0
+    
+    for n in range(termos):
+        sinal_formula = (-1)**n
+        numerador = radiano**(2*n)
+        denominador = fatorial(2*n)
+        termos = sinal_formula * (numerador / denominador)
+        cosseno += termos
+    return cosseno
 
